89d70a975bc72a5382342ad7eeae4210619f5856
42 <oncwnuCsdJh8HvF4L7XdzpEynXXY@git.weixin.qq.com>
2020-01-08 11:25:29 +0800

    First Commit CMS

---
+++ src/com/cms/util/RequestUtil.java
@@ -0,0 +1,38 @@
+package com.cms.util;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+/**
+ * 说明：请求处理工具
+ * 创建人：gzj
+ * 修改日期：2018/6/22
+ * @version
+ */
+
+public class RequestUtil {
+	
+	public static String getValue(){
+		HttpServletRequest request = ((ServletRequestAttributes)RequestContextHolder.getRequestAttributes()).getRequest();
+		PageData pd = new PageData(request);
+		
+		Map properties = request.getParameterMap();
+		Map returnMap = new HashMap(); 
+		Iterator entries = properties.entrySet().iterator(); 
+		Map.Entry entry; 
+		String name = "";  
+		String value = "";  
+		
+		while (entries.hasNext()) {
+			entry = (Map.Entry) entries.next(); 
+			name = (String) entry.getKey(); 
+		}
+		return name;
+	}
+
+}
